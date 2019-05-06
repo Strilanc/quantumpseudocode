3fcdb0ebf111d28233e454962bf893f50196fc4e
Craig Gidney <craig.gidney@gmail.com>
2019-05-05 23:35:05 -0700

    shorter

--- quantumpseudocode/control.py
+++ quantumpseudocode/control.py
@@ -1,12 +1,12 @@
 from typing import Union, Any, Optional
 
-import quantumpseudocode
+import quantumpseudocode as qp
 
 
-class ControlledRValue(quantumpseudocode.RValue):
+class ControlledRValue(qp.RValue):
     def __init__(self,
-                 controls: quantumpseudocode.QubitIntersection,
-                 rvalue: quantumpseudocode.RValue):
+                 controls: qp.QubitIntersection,
+                 rvalue: qp.RValue):
         self.controls = controls
         self.rvalue = rvalue
 
@@ -15,40 +15,40 @@ class ControlledRValue(quantumpseudocode.RValue):
 
     def init_storage_location(self,
                               location: Any,
-                              controls: 'quantumpseudocode.QubitIntersection'):
+                              controls: 'qp.QubitIntersection'):
         self.rvalue.init_storage_location(location, controls & self.controls)
 
     def del_storage_location(self,
                              location: Any,
-                            controls: 'quantumpseudocode.QubitIntersection'):
+                            controls: 'qp.QubitIntersection'):
         self.rvalue.del_storage_location(location, controls & self.controls)
 
     def __str__(self):
         return 'controlled({}, {})'.format(self.rvalue, self.controls)
 
     def __repr__(self):
-        return 'quantumpseudocode.ControlledRValue({!r}, {!r})'.format(self.controls,
+        return 'qp.ControlledRValue({!r}, {!r})'.format(self.controls,
                                                            self.rvalue)
 
 
 class _ControlledByWithoutRValue:
-    def __init__(self, controls: quantumpseudocode.QubitIntersection):
+    def __init__(self, controls: qp.QubitIntersection):
         self.controls = controls
 
     def __and__(self, other):
         if isinstance(other, ControlledRValue):
             return ControlledRValue(self.controls & other.controls, other.rvalue)
-        if isinstance(other, quantumpseudocode.RValue):
+        if isinstance(other, qp.RValue):
             return ControlledRValue(self.controls, other)
         if isinstance(other, int):
-            return ControlledRValue(self.controls, quantumpseudocode.IntRValue(other))
+            return ControlledRValue(self.controls, qp.IntRValue(other))
         return NotImplemented
 
     def __rand__(self, other):
         return self.__and__(other)
 
 
-def controlled_by(qubits: Union[quantumpseudocode.Qubit, quantumpseudocode.QubitIntersection]):
-    if isinstance(qubits, quantumpseudocode.Qubit):
-        qubits = quantumpseudocode.QubitIntersection((qubits,))
+def controlled_by(qubits: Union[qp.Qubit, qp.QubitIntersection]):
+    if isinstance(qubits, qp.Qubit):
+        qubits = qp.QubitIntersection((qubits,))
     return _ControlledByWithoutRValue(qubits)
