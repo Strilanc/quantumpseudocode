e05c77f8b6184e31a1c4273930150782418a8a50
Craig Gidney <craiggidney@google.com>
2019-05-06 23:19:22 -0700

    TimesEqual example

---
+++ examples/times_equal.py
@@ -0,0 +1,44 @@
+from quantumpseudocode import *
+
+
+def times_equal_builtin(target: Quint, k: int):
+    assert k & 1
+    target *= k
+
+
+def times_equal_classic(target: Quint, k: int):
+    assert k % 2 == 1  # Even factors aren't reversible.
+    k %= 2**len(target)  # Normalize factor.
+    for i in range(len(target))[::-1]:
+        with controlled_by(target[i]):
+            target[i + 1:] += k >> 1
+
+
+def times_equal_windowed(target: Quint, k: int, window: int):
+    assert k % 2 == 1  # Even factors aren't reversible.
+    k %= 2**len(target)  # Normalize factor.
+    if k == 1:
+        return
+    table = LookupTable([
+        (j * k) >> window
+        for j in range(2**window)
+    ])
+    for i in range(0, len(target), window)[::-1]:
+        w = target[i:i + window]
+        target[i + window:] += table[w]
+
+        # Recursively fix up the window.
+        times_equal_windowed(w, k, window=1)
+
+
+def times_equal_windowed_2(target: Quint, k: int):
+    times_equal_windowed(target, k, 2)
+
+
+def times_equal_windowed_4(target: Quint, k: int):
+    times_equal_windowed(target, k, 4)
+
+
+def times_equal_windowed_lg(target: Quint, k: int):
+    w = max(1, ceil_lg2(len(target)))
+    times_equal_windowed(target, k, w)
---
+++ examples/times_equal_test.py
@@ -0,0 +1,49 @@
+import functools
+import itertools
+import random
+
+import pytest
+
+import quantumpseudocode as qp
+from .times_equal import *
+
+
+methods = [
+    times_equal_classic,
+    times_equal_windowed_2,
+    times_equal_windowed_4,
+    times_equal_windowed_lg,
+]
+
+cases = [
+    {'n': 5, 't': 2, 'k': 3},
+    {
+        'n': 10,
+        't': random.randint(0, 2**10-1),
+        'k': random.randint(0, 2**10-1) | 1,
+    },
+    {
+        'n': 20,
+        't': random.randint(0, 2**20 - 1),
+        'k': random.randint(0, 2**20 - 1) | 1,
+    },
+]
+
+
+@pytest.mark.parametrize(
+    'method,case',
+    itertools.product(methods, cases)
+)
+def test_correct_result(method, case):
+    n = case['n']
+    t = case['t']
+    k = case['k']
+
+    final_state = qp.testing.sim_call(
+        method,
+        target=qp.IntBuf.raw(val=t, length=n),
+        k=k)
+    assert final_state == qp.ArgsAndKwargs([], {
+        'target': t * k % 2**n,
+        'k': k,
+    })
